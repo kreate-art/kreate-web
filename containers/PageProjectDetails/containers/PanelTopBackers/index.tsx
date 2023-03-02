336ff27a7bec9d2414412da186d61722a7a4c56a
Kitsune <kitsune@shinka.network>
2023-03-02 16:12:34 +0700

    refactor: rename PanelTopSupporters to PanelTopBackers

--- containers/PageProjectDetails/containers/PanelTopBackers/index.tsx
+++ containers/PageProjectDetails/containers/PanelTopBackers/index.tsx
@@ -4,17 +4,21 @@ import styles from "./index.module.scss";
 
 import { SupporterInfo } from "@/modules/business-types";
 import TableTopSupporters from "@/modules/teiki-components/components/TableTopSupporters";
+import Flex from "@/modules/teiki-ui/components/Flex";
+import Typography from "@/modules/teiki-ui/components/Typography";
 
 type Props = {
   className?: string;
   value: SupporterInfo[];
 };
 
-export default function PanelTopSupporters({ className, value }: Props) {
+export default function PanelTopBackers({ className, value }: Props) {
   return (
     <div className={cx(className, styles.container)}>
-      <h6 className={styles.label}>Top Backers</h6>
-      <TableTopSupporters value={value} />
+      <Flex.Col gap="24px">
+        <Typography.Div size="heading6" color="ink" content="Top Backers" />
+        <TableTopSupporters value={value} />
+      </Flex.Col>
     </div>
   );
 }
