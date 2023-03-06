6e3c8a1078f9d116f5669e2767be1a68f81af7cf
Kitsune <kitsune@shinka.network>
2023-03-06 19:33:30 +0800

    refactor: remove modules/storage

--- modules/with-bufs-as-converters/codecs/CodecBlob.ts
+++ modules/with-bufs-as-converters/codecs/CodecBlob.ts
@@ -1,7 +1,7 @@
 import { CodecError } from "../errors";
 import { Url, Codec } from "../types";
+import { arrayBufferToSha256 } from "../utils/buffer";
 
-import { arrayBufferToSha256 } from "@/modules/storage/utils/buffer";
 import { pure, WithBufsAs } from "@/modules/with-bufs-as";
 
 // NOTE: This file defines `CodecBlob`, which is similar to
@@ -13,6 +13,9 @@ async function fromUrl(url: Url): Promise<WithBufsAs<Url, Blob>> {
   const response = await fetch(url);
   const blob = await response.blob();
   const buf = await blob.arrayBuffer();
+  // TODO: @sk-kitsune: There is a more efficient way to do this
+  // The current implementation loads everything into RAM.
+  // Let's resolve later.
   const sha256 = await arrayBufferToSha256(buf);
   return { data: "sha256:" + sha256, bufs: { [sha256]: blob } };
 }
