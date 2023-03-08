ce0d6c666c53ac038ddec7ffbab40d90360ba46f
Kitsune <kitsune@shinka.network>
2023-03-08 20:08:30 +0800

    feat: add route /c-by-id/<projectId>/preview and redirect /projects-by-id/<projectId>/preview

---
+++ pages/c-by-id/[projectId]/preview.tsx
@@ -0,0 +1,27 @@
+import { GetServerSideProps } from "next";
+
+import PagePreviewProject from "../../../containers/PagePreviewProject";
+import { getStorageId } from "../../../containers/PageUpdateProjectV2/utils/storage";
+
+type Props = {
+  projectId: string;
+};
+
+export default function RouteToPagePreviewProject({ projectId }: Props) {
+  return (
+    <PagePreviewProject
+      storageId={getStorageId(projectId)}
+      projectId={projectId}
+    />
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const projectId = context.query["projectId"];
+  if (typeof projectId !== "string" || !/^[0-9A-Fa-f]+$/.test(projectId)) {
+    return { notFound: true };
+  }
+  return { props: { projectId } };
+};
--- pages/projects-by-id/[projectId]/preview.tsx
+++ pages/projects-by-id/[projectId]/preview.tsx
@@ -1,27 +1,20 @@
 import { GetServerSideProps } from "next";
 
-import PagePreviewProject from "../../../containers/PagePreviewProject";
-import { getStorageId } from "../../../containers/PageUpdateProjectV2/utils/storage";
-
-type Props = {
-  projectId: string;
-};
-
-export default function RouteToPagePreviewProject({ projectId }: Props) {
-  return (
-    <PagePreviewProject
-      storageId={getStorageId(projectId)}
-      projectId={projectId}
-    />
-  );
+export default function RouteToPagePreviewProject() {
+  return null;
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async (
-  context
-) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const projectId = context.query["projectId"];
+
   if (typeof projectId !== "string" || !/^[0-9A-Fa-f]*$/.test(projectId)) {
     return { notFound: true };
   }
-  return { props: { projectId } };
+
+  return {
+    redirect: {
+      permanent: true,
+      destination: `/c-by-id/${projectId}/preview`,
+    },
+  };
 };
