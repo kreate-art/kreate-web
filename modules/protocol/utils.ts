baa459ed724a412f4b157f55c97c7848e171f55d
Kitsune <kitsune@shinka.network>
2023-03-10 14:17:02 +0800

    refactor: remove unused padding constants

--- modules/protocol/utils.ts
+++ modules/protocol/utils.ts
@@ -1,8 +1,9 @@
 import { httpGetLatestBlock } from "./api/httpGetLatestBlock";
-import { TX_TIME_START_PADDING } from "./constants";
 
 import { UnixTimestamp } from "@/modules/business-types";
 import { writeErrorToConsole } from "@/modules/displayable-error";
+
+const FALLBACK_PADDING = 60000;
 
 export async function getTxTimeStart(): Promise<UnixTimestamp> {
   try {
@@ -11,14 +12,6 @@ export async function getTxTimeStart(): Promise<UnixTimestamp> {
   } catch (error) {
     writeErrorToConsole(error);
     const now = Math.floor(Date.now() / 1000) * 1000;
-    return now - TX_TIME_START_PADDING;
+    return now - FALLBACK_PADDING;
   }
-}
-
-// NOTE: @sk-kitsune: in the future, we will deprecate this function and
-// use getTxTimeStart directly
-export async function getTxTimeStartPadding(): Promise<number> {
-  const padding = Date.now() - (await getTxTimeStart());
-  // this is how ouroboros works
-  return Math.ceil(padding / 1000) * 1000;
 }
