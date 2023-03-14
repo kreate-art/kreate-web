00a3531ce4b5d360e6cb5d3697eae34493d70683
Shishi | Shinka <shishi@shinka.network>
2023-03-14 17:30:00 +0700

    feat: disable next.js image optimization for exclusive content

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,8 @@ const IPFS_GATEWAY_ORIGIN =
   process.env.NEXT_PUBLIC_IPFS_GATEWAY_ORIGIN ||
   "";
 
+const SAFE_HTTP_METHODS = new Set(["GET", "HEAD", "OPTIONS"]);
+
 if (!/^(http|https):\/\/.*[^/]$/.test(IPFS_GATEWAY_ORIGIN))
   throw new Error(
     "IPFS_GATEWAY_ORIGIN must starts with " +
@@ -15,30 +17,35 @@ console.log(`Interally route IPFS Gateway to: ${IPFS_GATEWAY_ORIGIN}`);
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const method = request.method;
-  if (method !== "GET" && method !== "HEAD" && method !== "OPTIONS")
-    return new NextResponse("GET / HEAD / OPTIONS only", {
-      status: 405,
-      statusText: "Method Not Allowed",
-    });
   const url = request.nextUrl;
   const pathname = url.pathname;
-  if (pathname === "/_proxy") {
-    const proxyUrl = url.searchParams.get("url");
-    if (!proxyUrl)
-      return new NextResponse("URL must be specified", {
-        status: 400,
-        statusText: "Bad Request",
-      });
-    return NextResponse.rewrite(proxyUrl);
+  if (pathname === "/_next/image") {
+    const imageUrl = url.searchParams.get("url");
+    if (imageUrl && imageUrl.includes("/api/v1/exclusive"))
+      return NextResponse.redirect(new URL(imageUrl, request.url), 308);
   } else {
-    // Must be /_ipfs/:path*
-    return NextResponse.rewrite(
-      `${IPFS_GATEWAY_ORIGIN}${pathname.slice(6)}${url.search}${url.hash}`
-    );
+    if (!SAFE_HTTP_METHODS.has(request.method))
+      return new NextResponse("Invalid HTTP method", {
+        status: 405,
+        statusText: "Method Not Allowed",
+      });
+    if (pathname === "/_proxy") {
+      const proxyUrl = url.searchParams.get("url");
+      if (!proxyUrl)
+        return new NextResponse("URL must be specified", {
+          status: 400,
+          statusText: "Bad Request",
+        });
+      return NextResponse.rewrite(proxyUrl);
+    } else {
+      // Must be /_ipfs/:path*
+      return NextResponse.rewrite(
+        `${IPFS_GATEWAY_ORIGIN}${pathname.slice(6)}${url.search}${url.hash}`
+      );
+    }
   }
 }
 
 export const config = {
-  matcher: ["/_ipfs/:path*", "/_proxy"],
+  matcher: ["/_next/image", "/_ipfs/:path*", "/_proxy"],
 };
