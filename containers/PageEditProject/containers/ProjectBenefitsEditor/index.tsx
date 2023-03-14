2089f91b0ced645bc85795f485b3ef6c8d842162
Umiuma <umiuma@shinka.network>
2023-03-14 17:51:48 +0700

    feat: remove benefits tabs

--- containers/PageEditProject/containers/ProjectBenefitsEditor/index.tsx
+++ containers/PageEditProject/containers/ProjectBenefitsEditor/index.tsx
@@ -1,15 +1,3 @@
-import GrammarlyWrapper from "../../../../components/GrammarlyWrapper";
-
-import styles from "./index.module.scss";
-
-import { ProjectBenefits } from "@/modules/business-types";
-import RichTextEditor from "@/modules/teiki-components/components/RichTextEditor";
-
-type Props = {
-  value: ProjectBenefits;
-  onChange?: (newValue: ProjectBenefits) => void;
-};
-
 /**
  * `ProjectBenefitsEditor` is the editor of `ProjectBenefits`.
  *
@@ -17,18 +5,6 @@ type Props = {
  * triggers `onChange(newValue)` on every change.
  */
 
-export default function ProjectBenefitsEditor({ value, onChange }: Props) {
-  return (
-    <div className={styles.container}>
-      <GrammarlyWrapper>
-        <RichTextEditor
-          value={value.perks}
-          onChange={(newPerks) => {
-            onChange && onChange({ ...value, perks: newPerks });
-          }}
-          isBorderless={true}
-        />
-      </GrammarlyWrapper>
-    </div>
-  );
+export default function ProjectBenefitsEditor() {
+  return null;
 }
--- modules/next-backend/logic/getDetailedProject.ts
+++ modules/next-backend/logic/getDetailedProject.ts
@@ -405,10 +405,5 @@ async function backingDataToActivities(
 
 // TODO: Clean up this later
 function emptyProjectBenefits(): ProjectBenefits {
-  return {
-    perks: {
-      type: "doc",
-      content: [{ type: "paragraph" }],
-    },
-  };
+  return { tiers: [] };
 }
