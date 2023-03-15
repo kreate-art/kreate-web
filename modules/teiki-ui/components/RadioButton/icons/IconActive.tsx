2daae636d34785fa91945aae163977b2d58520a8
Umiuma | Shinka <110230002+sk-umiuma@users.noreply.github.com>
2023-03-16 00:22:32 +0700

    feat: exclusive post UI (#70)

---
+++ modules/teiki-ui/components/RadioButton/icons/IconActive.tsx
@@ -0,0 +1,13 @@
+export default function IconActive() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="12" height="12" rx="6" fill="white" />
+    </svg>
+  );
+}
