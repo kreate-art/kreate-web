1952fe57d15bfc837125cda58c6ff674bc52ca16
Umiuma | Shinka <110230002+sk-umiuma@users.noreply.github.com>
2023-03-16 13:11:25 +0700

    feat: exclusive tiers, create project flow (#79)
    * feat: exclusive tiers, create project flow
    
    * feat: move tiers to a separate project field
    
    * fix: tier required stake input

---
+++ containers/PageEditProject/containers/ProjectBenefitsEditor/components/TierInput/icons/IconDrag.tsx
@@ -0,0 +1,72 @@
+export default function IconDrag() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 18C14 17.4477 14.4477 17 15 17C15.5523 17 16 17.4477 16 18C16 18.5523 15.5523 19 15 19C14.4477 19 14 18.5523 14 18Z"
+        fill="#00362C"
+        fillOpacity="0.5"
+        stroke="#00362C"
+        strokeOpacity="0.5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 18C8 17.4477 8.44772 17 9 17C9.55228 17 10 17.4477 10 18C10 18.5523 9.55228 19 9 19C8.44772 19 8 18.5523 8 18Z"
+        fill="#00362C"
+        fillOpacity="0.5"
+        stroke="#00362C"
+        strokeOpacity="0.5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 12C14 11.4477 14.4477 11 15 11C15.5523 11 16 11.4477 16 12C16 12.5523 15.5523 13 15 13C14.4477 13 14 12.5523 14 12Z"
+        fill="#00362C"
+        fillOpacity="0.5"
+        stroke="#00362C"
+        strokeOpacity="0.5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 12C8 11.4477 8.44772 11 9 11C9.55228 11 10 11.4477 10 12C10 12.5523 9.55228 13 9 13C8.44772 13 8 12.5523 8 12Z"
+        fill="#00362C"
+        fillOpacity="0.5"
+        stroke="#00362C"
+        strokeOpacity="0.5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 6C14 5.44772 14.4477 5 15 5C15.5523 5 16 5.44772 16 6C16 6.55228 15.5523 7 15 7C14.4477 7 14 6.55228 14 6Z"
+        fill="#00362C"
+        fillOpacity="0.5"
+        stroke="#00362C"
+        strokeOpacity="0.5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 6C8 5.44772 8.44772 5 9 5C9.55228 5 10 5.44772 10 6C10 6.55228 9.55228 7 9 7C8.44772 7 8 6.55228 8 6Z"
+        fill="#00362C"
+        fillOpacity="0.5"
+        stroke="#00362C"
+        strokeOpacity="0.5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ containers/PageEditProject/containers/ProjectBenefitsEditor/icons/IconPlusSquare.tsx
@@ -0,0 +1,13 @@
+export default function IconPlusSquare() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5 20h14c.6 0 1-.4 1-1V5c0-.6-.4-1-1-1H5a1 1 0 0 0-1 1v14c0 .6.4 1 1 1ZM8 12h8M12 16V8"
+      />
+    </svg>
+  );
+}
