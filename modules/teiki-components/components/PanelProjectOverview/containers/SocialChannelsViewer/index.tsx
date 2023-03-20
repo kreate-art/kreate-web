78345d39511f4aeeaef61a36b5a97d36c70ae764
Kitsune <kitsune@shinka.network>
2023-03-20 22:41:25 +0700

    refactor: simplify SocialChannelsViewer

--- modules/teiki-components/components/PanelProjectOverview/containers/SocialChannelsViewer/index.tsx
+++ modules/teiki-components/components/PanelProjectOverview/containers/SocialChannelsViewer/index.tsx
@@ -3,11 +3,10 @@ import * as React from "react";
 
 import Flex from "../../components/Flex";
 
+import ButtonSocialChannel from "./components/ButtonSocialChannel";
 import styles from "./index.module.scss";
-import { formatToEntry } from "./utils";
 
 import { ProjectCommunity } from "@/modules/business-types";
-import Button from "@/modules/teiki-ui/components/Button";
 
 type Props = {
   className?: string;
@@ -20,44 +19,20 @@ export default function SocialChannelsViewer({
   style,
   value,
 }: Props) {
-  const entries = value.map((item) => formatToEntry(item));
-
   return (
     <div className={cx(styles.container, className)} style={style}>
-      <Flex.Row gap="12px" style={{ marginTop: "-4px", marginBottom: "-4px" }}>
+      <Flex.Row gap="12px">
         <Flex.Cell className={styles.label} flex="0 0 auto">
           {"Social: "}
         </Flex.Cell>
-        <Flex.Row gap="4px" flexWrap="wrap">
-          {!entries.length ? (
+        <Flex.Row gap="12px" flexWrap="wrap">
+          {!value.length ? (
             <span style={{ lineHeight: "40px", color: "rgba(0, 0, 0, 0.6)" }}>
               {"-"}
             </span>
           ) : (
-            entries.map(({ url, isSafe, icon: Icon, tooltip }, index) => (
-              <a
-                key={index}
-                // NOTE: @sk-kitsune: this hack is used because we don't have Button.Outline size="extraSmall" yet
-                style={{ transform: "scale(0.8)" }}
-                href={url}
-                onClick={(event) => {
-                  if (isSafe) return;
-                  if (confirm("Are you sure to open this link?\n" + url))
-                    return;
-                  event.preventDefault();
-                }}
-                target="_blank"
-                rel="noopener noreferrer"
-                title={tooltip}
-              >
-                <Button.Outline
-                  as="div"
-                  icon={<Icon width="24" height="24" />}
-                  color="green"
-                  size="small"
-                  circular
-                />
-              </a>
+            value.map((item, index) => (
+              <ButtonSocialChannel key={index} value={item} />
             ))
           )}
         </Flex.Row>
