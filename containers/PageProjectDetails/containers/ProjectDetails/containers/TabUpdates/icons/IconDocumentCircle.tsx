e7be863b18168aeab35ea2e70dd5e1b67e81d59f
sk-yagi <yagi@shinka.network>
2023-03-20 17:58:32 +0700

    feat: new logo post

--- containers/PageProjectDetails/containers/ProjectDetails/containers/TabUpdates/icons/IconDocumentCircle.tsx
+++ containers/PageProjectDetails/containers/ProjectDetails/containers/TabUpdates/icons/IconDocumentCircle.tsx
@@ -6,22 +6,41 @@ export default function IconDocumentCircle({
   return (
     <svg
       className={className}
-      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="49"
+      viewBox="0 0 48 49"
       fill="none"
-      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="24" cy="24" r="24" fill="#008A45" />
-      <g clipPath="url(#a)">
-        <path
-          fill="#fff"
-          d="M28 15H17a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V20l-5-5Zm-9 4h5v2h-5v-2Zm10 10H19v-2h10v2Zm0-4H19v-2h10v2Zm-2-4v-4l4 4h-4Z"
-        />
-      </g>
-      <defs>
-        <clipPath id="a">
-          <path fill="#fff" d="M12 12h24v24H12z" />
-        </clipPath>
-      </defs>
+      <circle cx="24" cy="24.5" r="24" fill="#222222" />
+      <path
+        d="M30 34H18C17.4477 34 17 33.5523 17 33L17 17C17 16.4477 17.4477 16 18 16L25.5631 16C25.8416 16 26.1076 16.1162 26.2968 16.3206L30.7338 21.1125C30.9049 21.2973 31 21.5399 31 21.7919L31 33C31 33.5523 30.5523 34 30 34Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21 30H27"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21 27H27"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M31 22L26 22C25.4477 22 25 21.5523 25 21L25 16"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 }
