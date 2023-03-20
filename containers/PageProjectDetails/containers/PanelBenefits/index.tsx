f254916dff0cfea846a5bc32eb27db8af3e42dda
Umiuma <umiuma@shinka.network>
2023-03-20 12:33:05 +0700

    feat: tiers section grid

--- containers/PageProjectDetails/containers/PanelBenefits/index.tsx
+++ containers/PageProjectDetails/containers/PanelBenefits/index.tsx
@@ -1,14 +1,12 @@
 import cx from "classnames";
 import React from "react";
 
-import Tier from "./containers/Tier";
+import TierViewer from "./containers/TierViewer";
 import styles from "./index.module.scss";
 
-import { joinWithSeparator } from "@/modules/array-utils";
 import { ProjectBenefitsTier } from "@/modules/business-types";
 import { useElementSize } from "@/modules/common-hooks/hooks/useElementSize";
 import Button from "@/modules/teiki-ui/components/Button";
-import Divider from "@/modules/teiki-ui/components/Divider";
 import Flex from "@/modules/teiki-ui/components/Flex";
 
 type Props = {
@@ -29,7 +27,6 @@ export default function PanelBenefits({
     React.useState<HTMLDivElement | null>(null);
   const containerSize = useElementSize(containerElement);
   const numVisibleItems = containerSize ? Math.ceil(containerSize.w / 600) : 1;
-  const items = partition(value, numVisibleItems);
 
   return (
     <div
@@ -38,31 +35,12 @@ export default function PanelBenefits({
       ref={setContainerElement}
     >
       <Flex.Col gap="12px" className={styles.box}>
-        {(showAll ? items : [items[0]]).map((tiers, index) => (
-          <Flex.Row className={styles.row} key={index}>
-            {joinWithSeparator(
-              tiers.map((tier, index) => (
-                <Tier
-                  value={tier}
-                  key={index}
-                  style={{
-                    flex: "1 1",
-                    // NOTE: The subtracted pixels are meant to make space for the dividers
-                    minWidth: `calc(${100 / numVisibleItems}% - ${
-                      numVisibleItems - 1
-                    }px)`,
-                    maxWidth: `calc(${100 / numVisibleItems}% - ${
-                      numVisibleItems - 1
-                    }px)`,
-                  }}
-                  onClickBecomeMember={onClickBecomeMember}
-                />
-              )),
-              <Divider.Vertical />
-            )}
-          </Flex.Row>
-        ))}
-        {showAll || items.length < 2 ? null : (
+        <TierViewer
+          value={showAll ? value : value.slice(0, numVisibleItems)}
+          numColumn={numVisibleItems}
+          onClickBecomeMember={onClickBecomeMember}
+        />
+        {showAll || value.length <= numVisibleItems ? null : (
           <Button.Link
             content={`See All ${value.length} tiers`}
             onClick={() => setShowAll(true)}
@@ -72,17 +50,4 @@ export default function PanelBenefits({
       </Flex.Col>
     </div>
   );
-}
-
-function partition(
-  value: ProjectBenefitsTier[],
-  length: number
-): ProjectBenefitsTier[][] {
-  const result: ProjectBenefitsTier[][] = [];
-  let items = value;
-  while (items.length) {
-    result.push(items.slice(0, length));
-    items = items.slice(length);
-  }
-  return result;
 }
