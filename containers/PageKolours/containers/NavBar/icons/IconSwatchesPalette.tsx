aef9f9380648830601a557b9f56f5645fd088a4b
sk-yagi <yagi@shinka.network>
2023-03-23 00:56:46 +0700

    feat: improve Kolours NavBar

---
+++ containers/PageKolours/containers/NavBar/icons/IconSwatchesPalette.tsx
@@ -0,0 +1,27 @@
+export default function IconSwatchesPalette() {
+  return (
+    <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M9.882 18.405 13.498 4.91a.53.53 0 0 0-.375-.648L7.384 2.724a.53.53 0 0 0-.648.375L3.12 16.593a3.5 3.5 0 1 0 6.762 1.812Z"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.5 21h13.97a.53.53 0 0 0 .53-.53v-5.94M6.5 17.6h.002v.002H6.5V17.6Z"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="m7.98 20.671 12.662-5.904a.53.53 0 0 0 .256-.704l-2.51-5.384a.53.53 0 0 0-.704-.256l-5.654 2.636"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
