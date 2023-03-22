5ca841787ddbfa7af3572eb22dd0b12970dc993d
Shishi | Shinka <shishi@shinka.network>
2023-03-22 23:54:30 +0700

    feat: env vars for genesis kreation

--- modules/env/kolours/server.ts
+++ modules/env/kolours/server.ts
@@ -4,14 +4,39 @@ import { parseEnv$Optional } from "../utils/wrappers";
 
 import { createSecretKey } from "@/modules/crypt";
 
-export const KOLOURS_FEE_ADDRESS = IS_NEXT_BUILD
+const parsePrivateKey = parseStringByRegex(/^ed25519_sk[0-9A-Za-z]+$/);
+
+export const KOLOURS_KOLOUR_NFT_FEE_ADDRESS = IS_NEXT_BUILD
   ? never()
   : parseEnv$Optional({
-      label: "KOLOURS_FEE_ADDRESS",
-      input: process.env.KOLOURS_FEE_ADDRESS,
+      label: "KOLOURS_KOLOUR_NFT_FEE_ADDRESS",
+      input: process.env.KOLOURS_KOLOUR_NFT_FEE_ADDRESS,
       parser: parseStringByRegex(/^addr_[0-9a-z]+$/),
       defaultValue: null,
     });
+
+export const KOLOURS_KOLOUR_NFT_PRIVATE_KEY = parseEnv$Optional({
+  label: "KOLOURS_KOLOUR_NFT_PRIVATE_KEY",
+  input: process.env.KOLOURS_KOLOUR_NFT_PRIVATE_KEY,
+  parser: parsePrivateKey,
+  defaultValue: null,
+});
+
+export const KOLOURS_GENESIS_KREATION_FEE_ADDRESS = IS_NEXT_BUILD
+  ? never()
+  : parseEnv$Optional({
+      label: "KOLOURS_GENESIS_KREATION_FEE_ADDRESS",
+      input: process.env.KOLOURS_GENESIS_KREATION_FEE_ADDRESS,
+      parser: parseStringByRegex(/^addr_[0-9a-z]+$/),
+      defaultValue: null,
+    });
+
+export const KOLOURS_GENESIS_KREATION_PRIVATE_KEY = parseEnv$Optional({
+  label: "KOLOURS_GENESIS_KREATION_PRIVATE_KEY",
+  input: process.env.KOLOURS_GENESIS_KREATION_PRIVATE_KEY,
+  parser: parsePrivateKey,
+  defaultValue: null,
+});
 
 export const KOLOURS_HMAC_SECRET = IS_NEXT_BUILD
   ? never()
@@ -22,10 +47,3 @@ export const KOLOURS_HMAC_SECRET = IS_NEXT_BUILD
         createSecretKey("hmac", parseStringByRegex(/^[ -~]+$/)(text)),
       defaultValue: null,
     });
-
-export const KOLOURS_PRODUCER_PRIVATE_KEY = parseEnv$Optional({
-  label: "KOLOURS_PRODUCER_PRIVATE_KEY",
-  input: process.env.KOLOURS_PRODUCER_PRIVATE_KEY,
-  parser: parseStringByRegex(/^ed25519_sk[0-9A-Za-z]+$/),
-  defaultValue: null,
-});
