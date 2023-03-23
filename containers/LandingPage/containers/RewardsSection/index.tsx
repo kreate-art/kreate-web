daa890c00e92802c45f8e14b1ccf6a4f00e7dc63
Tenba <tenba@shinka.network>
2023-03-24 00:49:30 +0700

    fix: landing page first draft

--- containers/LandingPage/containers/RewardsSection/index.tsx
+++ containers/LandingPage/containers/RewardsSection/index.tsx
@@ -1,7 +1,15 @@
 import cx from "classnames";
+import Image from "next/image";
+import Link from "next/link";
 
+import IconSwatches from "../NftCollection/components/IconSwatches";
+
+import humanFigure1 from "./images/human-figure-1.png";
+import humanFigure2 from "./images/human-figure-2.png";
+import kreationNft from "./images/kreation-nft.png";
 import styles from "./index.module.scss";
 
+import ImageView from "@/modules/teiki-components/components/ImageView";
 import Typography from "@/modules/teiki-ui/components/Typography";
 
 type Props = {
@@ -26,59 +34,141 @@ export default function RewardSection({ className, style }: Props) {
     <div className={className} style={style}>
       <div className={styles.rewardsTitle}>Your Rewards</div>
       <div className={styles.gridContainer}>
-        <div className={styles.grid} style={{ gridArea: "a" }}>
-          <Typography.Div size="heading2" color="orange" content="Kolour NFT" />
-          <div className={styles.kolourList}>
-            {kolours.map((kolour) => (
-              <div
-                key={kolour}
-                className={styles.kolour}
-                style={{ backgroundColor: kolour }}
-              />
-            ))}
-          </div>
-          <div className={styles.infoCardContainer}>
-            <div className={cx(styles.infoCard, styles.gray)}>
-              <Typography.Div
-                size="heading3"
-                color="white"
-                content="Kolour the Kreataverse"
-              />
-              <div className={styles.infoBody}>
-                Only minted kolours can be used for Kreator studio, merch and
-                new NFT collections.
+        <div className={styles.upperGrid}>
+          <div className={styles.grid}>
+            <Typography.Div
+              size="heading2"
+              color="orange"
+              content="Kolour NFT"
+            />
+            <div className={styles.kolourList}>
+              {kolours.map((kolour) => (
+                <div
+                  key={kolour}
+                  className={styles.kolour}
+                  style={{ backgroundColor: kolour }}
+                />
+              ))}
+            </div>
+            <div className={styles.infoCardContainer}>
+              <div className={cx(styles.infoCard, styles.gray)}>
+                <Typography.Div
+                  size="heading3"
+                  color="white"
+                  content="Kolour the Kreataverse"
+                />
+                <div className={styles.infoBody}>
+                  Only minted kolours can be used for Kreator studio, merch and
+                  new NFT collections.
+                </div>
+              </div>
+              <div className={cx(styles.infoCard, styles.green)}>
+                <Typography.Div
+                  size="heading3"
+                  color="white"
+                  content="Royalty"
+                />
+                <div className={styles.infoBody}>
+                  Get a % minting fee from future NFTs that use your kolours.
+                </div>
+              </div>
+              <div className={cx(styles.infoCard, styles.gray)}>
+                <Typography.Div
+                  size="heading3"
+                  color="white"
+                  content="Early Kreator NFT"
+                />
+                <div className={styles.infoBody}>
+                  Kolour NFT holders are eligible for early-minting a Kreator
+                  NFT.
+                </div>
               </div>
             </div>
-            <div className={cx(styles.infoCard, styles.gray)}>
-              <Typography.Div
-                size="heading3"
-                color="white"
-                content="Kolour the Kreataverse"
+          </div>
+          <div className={styles.grid} style={{ gridArea: "b" }}>
+            <Typography.Div
+              size="heading2"
+              color="orange"
+              content="Genesis Kreation NFT"
+            />
+            <div className={styles.kreationNftContainer}>
+              <ImageView
+                src={kreationNft.src}
+                className={styles.imageView}
+                crop={{ x: 0, y: 0, w: 1, h: 1 }}
               />
-              <div className={styles.infoBody}>
-                Only minted kolours can be used for Kreator studio, merch and
-                new NFT collections.
+            </div>
+            <div className={styles.infoCardContainer}>
+              <div className={cx(styles.infoCard, styles.gray)}>
+                <Typography.Div
+                  size="heading3"
+                  color="white"
+                  content="Kreate your land"
+                />
+                <div className={styles.infoBody}>
+                  Name and develop the land you own in the Kreataverse.
+                </div>
+              </div>
+              <div className={cx(styles.infoCard, styles.green)}>
+                <Typography.Div
+                  size="heading3"
+                  color="white"
+                  content="Royalty"
+                />
+                <div className={styles.infoBody}>
+                  Get a % revenue share from future Kreator studios and
+                  businesses on your land.
+                </div>
+              </div>
+              <div className={cx(styles.infoCard, styles.gray)}>
+                <Typography.Div
+                  size="heading3"
+                  color="white"
+                  content="Free Kreator NFT"
+                />
+                <div className={styles.infoBody}>
+                  Genesis Kreation NFT holders get a FREE Kreator NFT.
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <Typography.Div size="heading3" color="white" content="Royalty" />
-          </div>
-          <div>
-            <div>Early Kreator NFT</div>
-          </div>
         </div>
-        <div className={styles.grid} style={{ gridArea: "b" }}>
-          <Typography.Div
-            size="heading2"
-            color="orange"
-            content="Genesis Kreation NFT"
-          />
-        </div>
-        <div className={styles.grid} style={{ gridArea: "c" }}>
-          <Typography.Div color="orange" content="Genesis Kreation NFT" />
+        <div
+          className={cx(styles.grid, styles.bottomGrid)}
+          style={{ gridArea: "c" }}
+        >
+          <div className={styles.figureContainer}>
+            <div>
+              <Image
+                className={styles.figure}
+                src={humanFigure1}
+                alt="human-figure-1"
+              />
+            </div>
+            <div>
+              <Image
+                className={styles.figure}
+                src={humanFigure2}
+                alt="human-figure-2"
+              />
+            </div>
+          </div>
+          <div className={styles.infoBottomGrid}>
+            Kolour and Genesis Kreation NFT holders will{" "}
+            <b>get a $KREATE airdrop</b>
+            proportionally to their holding value. $KREATE is the utility token
+            of the <u>Kreate platform</u> and is highly beneficial to Kreators
+            and Members. Follow us on Twitter and Discord for the $KREATE
+            campaign in April 2023.
+          </div>
         </div>
       </div>
+      <Link href="/kolours" className={styles.kolourButtonContainer}>
+        <div className={styles.kolourButton}>
+          <IconSwatches />
+          <Typography.Span size="heading5" content="Farm the Kreataverse" />
+        </div>
+      </Link>
     </div>
   );
 }
