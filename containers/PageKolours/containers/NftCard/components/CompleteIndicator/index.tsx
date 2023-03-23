3c834fc985f6cbb5c9eedb3e416de89e96750b3e
Umiuma <umiuma@shinka.network>
2023-03-23 11:26:42 +0700

    feat: improve NftCardGrid

---
+++ containers/PageKolours/containers/NftCard/components/CompleteIndicator/index.tsx
@@ -0,0 +1,52 @@
+import cx from "classnames";
+import { PieChart } from "react-minimal-pie-chart";
+
+import { PaletteItem } from "../../../../kolours-types";
+
+import styles from "./index.module.scss";
+
+import Flex from "@/modules/teiki-ui/components/Flex";
+import Typography from "@/modules/teiki-ui/components/Typography";
+
+type Props = {
+  className?: string;
+  style?: React.CSSProperties;
+  value: PaletteItem[];
+};
+
+export default function CompleteIndicator({ className, style, value }: Props) {
+  const mintedCount = value.filter((item) => !!item.minted).length;
+  const pieChartData = [
+    { value: mintedCount, color: "#006e46" },
+    { value: value.length - mintedCount, color: "rgba(34, 34, 34, .1)" },
+  ];
+
+  return (
+    <div className={cx(styles.container, className)} style={style}>
+      <PieChart
+        data={pieChartData}
+        lineWidth={60}
+        labelPosition={70}
+        labelStyle={{
+          fill: "#fff",
+          opacity: 0.75,
+          pointerEvents: "none",
+          fontSize: "6px",
+        }}
+        startAngle={-90}
+      />
+      <Flex.Col
+        className={styles.percentage}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography.Div
+          content={`${Math.round((mintedCount * 100) / value.length)}%`}
+          size="bodyExtraSmall"
+          fontWeight="semibold"
+          color="ink80"
+        />
+      </Flex.Col>
+    </div>
+  );
+}
