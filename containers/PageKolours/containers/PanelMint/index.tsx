fdc372da13942e08385b0b71b098ad0f5720f6e7
Kitsune <kitsune@shinka.network>
2023-03-24 01:18:58 +0700

    feat: add button left and right

--- containers/PageKolours/containers/PanelMint/index.tsx
+++ containers/PageKolours/containers/PanelMint/index.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 
 import Palette from "./components/Palette";
 import Viewer from "./components/Viewer";
+import IconChevronLeft from "./icons/IconChevronLeft";
+import IconChevronRight from "./icons/IconChevronRight";
 import styles from "./index.module.scss";
 import { Selection } from "./types";
 
@@ -20,6 +22,10 @@ type Props = {
   palette: Kolours.Layer[] | undefined;
   fee: LovelaceAmount | undefined;
   listedFee: LovelaceAmount | undefined;
+  canGoPrev?: boolean;
+  onGoPrev?: () => void;
+  canGoNext?: boolean;
+  onGoNext?: () => void;
 };
 
 export default function PanelMint({
@@ -29,26 +35,47 @@ export default function PanelMint({
   palette,
   fee,
   listedFee,
+  canGoPrev,
+  onGoPrev,
+  canGoNext,
+  onGoNext,
 }: Props) {
   const [selection, setSelection] = React.useState<Selection>({});
   return (
     <div className={cx(styles.container, className)} style={style}>
       <Flex.Col gap="16px" paddingBottom="32px">
-        <Viewer
-          className={styles.viewer}
-          initialImage={initialImage}
-          palette={palette}
-          selectedIndexes={
-            palette
-              ? range(palette.length).filter(
-                  (index) =>
-                    palette[index].status !== "free" || !!selection[index]
-                )
-              : undefined
-          }
-          fee={fee}
-          listedFee={listedFee}
-        />
+        <div className={styles.viewerContainer}>
+          <Viewer
+            initialImage={initialImage}
+            palette={palette}
+            selectedIndexes={
+              palette
+                ? range(palette.length).filter(
+                    (index) =>
+                      palette[index].status !== "free" || !!selection[index]
+                  )
+                : undefined
+            }
+            fee={fee}
+            listedFee={listedFee}
+          />
+          {canGoPrev ? (
+            <button
+              className={cx(styles.buttonNavigate, styles.buttonNavigateLeft)}
+              onClick={onGoPrev}
+            >
+              <IconChevronLeft />
+            </button>
+          ) : null}
+          {canGoNext ? (
+            <button
+              className={cx(styles.buttonNavigate, styles.buttonNavigateRight)}
+              onClick={onGoNext}
+            >
+              <IconChevronRight />
+            </button>
+          ) : null}
+        </div>
         <Palette
           className={styles.palette}
           palette={palette}
