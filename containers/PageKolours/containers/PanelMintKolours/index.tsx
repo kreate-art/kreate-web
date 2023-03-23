caca55cc3f22bba62261e7d7f4ada7233f41ac0c
Kitsune <kitsune@shinka.network>
2023-03-23 11:01:03 +0700

    feat: allow selection and add mint button

--- containers/PageKolours/containers/PanelMintKolours/index.tsx
+++ containers/PageKolours/containers/PanelMintKolours/index.tsx
@@ -8,6 +8,7 @@ import { Image, PaletteItem } from "../../kolours-types";
 import PaletteCell from "./components/PaletteCell";
 import styles from "./index.module.scss";
 
+import Button from "@/modules/teiki-ui/components/Button";
 import Flex from "@/modules/teiki-ui/components/Flex";
 
 type Props = {
@@ -23,28 +24,60 @@ export default function PanelMintKolours({
   grayscaleImage,
   palette,
 }: Props) {
+  const [selection, setSelection] = React.useState<Record<number, boolean>>({});
+
   return (
     <div className={cx(styles.container, className)} style={style}>
-      <Flex.Row flexWrap="wrap" padding="24px" gap="32px" alignItems="stretch">
-        <Flex.Cell flex="1 1 300px">
-          <WithAspectRatio aspectRatio={1}>
-            <Image$Next src={grayscaleImage.src} alt="" fill />
-          </WithAspectRatio>
-        </Flex.Cell>
-        <Flex.Cell flex="1 1 300px">
-          <div className={styles.grid}>
-            {palette.map((item, index) => (
-              <PaletteCell
-                key={index}
-                color={item.color}
-                minted={item.minted}
-                checked={false}
-                fill
-              />
-            ))}
-          </div>
-        </Flex.Cell>
-      </Flex.Row>
+      <Flex.Col>
+        <Flex.Row
+          flexWrap="wrap"
+          padding="24px"
+          gap="32px"
+          alignItems="stretch"
+        >
+          <Flex.Cell flex="1 1 300px">
+            <WithAspectRatio aspectRatio={1}>
+              <Image$Next src={grayscaleImage.src} alt="" fill />
+              {palette.map((item, index) => (
+                <Image$Next
+                  style={{
+                    visibility: selection[index] ? "visible" : "hidden",
+                  }}
+                  key={index}
+                  src={item.image.src}
+                  alt=""
+                  fill
+                />
+              ))}
+            </WithAspectRatio>
+          </Flex.Cell>
+          <Flex.Cell flex="1 1 300px">
+            <div className={styles.grid}>
+              {palette.map((item, index) => (
+                <PaletteCell
+                  key={index}
+                  color={item.color}
+                  minted={item.minted}
+                  checked={selection[index]}
+                  fill
+                  onCheckedChange={(checked) => {
+                    setSelection((selection) => ({
+                      ...selection,
+                      [index]: checked,
+                    }));
+                  }}
+                />
+              ))}
+            </div>
+          </Flex.Cell>
+        </Flex.Row>
+        <Button.Solid
+          content="Mint"
+          onClick={() => {
+            alert(JSON.stringify(selection));
+          }}
+        />
+      </Flex.Col>
     </div>
   );
 }
