be77c65d10f0dabdcd575bcb48e0b052f35c774d
Umiuma <umiuma@shinka.network>
2023-03-23 11:26:42 +0700

    feat: improve NftCardGrid

--- containers/PageKolours/containers/NftCardGrid/index.tsx
+++ containers/PageKolours/containers/NftCardGrid/index.tsx
@@ -6,6 +6,13 @@ import NftCard from "../NftCard";
 
 import styles from "./index.module.scss";
 
+import { sortedBy } from "@/modules/array-utils";
+import { useElementSize } from "@/modules/common-hooks/hooks/useElementSize";
+import Button from "@/modules/teiki-ui/components/Button";
+import Divider$Horizontal$CustomDash from "@/modules/teiki-ui/components/Divider$Horizontal$CustomDash";
+import Flex from "@/modules/teiki-ui/components/Flex";
+import Typography from "@/modules/teiki-ui/components/Typography";
+
 type Props = {
   className?: string;
   style?: React.CSSProperties;
@@ -19,10 +26,54 @@ export default function NftCardGrid({
   value,
   onSelect,
 }: Props) {
+  const [containerElement, setContainerElement] =
+    React.useState<HTMLDivElement | null>(null);
+  const containerSize = useElementSize(containerElement);
+  const numColumn = containerSize ? Math.ceil(containerSize.w / 600) : 1;
+
+  const [sortIndex, setSortIndex] = React.useState(0);
+  const sortedValue =
+    sortIndex === 0
+      ? sortedBy(
+          value,
+          (nft) =>
+            -nft.palette.filter((item) => !!item.minted).length /
+            nft.palette.length
+        )
+      : value;
+
   return (
-    <div className={cx(styles.container, className)} style={style}>
-      <div className={styles.grid}>
-        {value.map((item) => (
+    <div
+      className={cx(styles.container, className)}
+      style={style}
+      ref={setContainerElement}
+    >
+      <Flex.Row justifyContent="space-between" style={{ marginBottom: "16px" }}>
+        <Flex.Row gap="8px" alignItems="center">
+          <Typography.Span
+            content={value.length}
+            fontWeight="bold"
+            size="heading4"
+          />
+          <Typography.Span content="NFTs" size="heading5" />
+        </Flex.Row>
+        <Flex.Row gap="8px" alignItems="center">
+          <Button.Solid
+            content="Completion"
+            size="small"
+            onClick={() => setSortIndex(0)}
+          />
+          <Button.Outline content="Latest" size="small" disabled={true} />
+          <Button.Outline content="Oldest" size="small" disabled={true} />
+        </Flex.Row>
+      </Flex.Row>
+      <Divider$Horizontal$CustomDash style={{ marginBottom: "48px" }} />
+
+      <div
+        className={styles.grid}
+        style={{ gridTemplateColumns: `repeat(${numColumn}, 1fr)` }}
+      >
+        {sortedValue.map((item) => (
           <NftCard
             key={item.id}
             value={item}
