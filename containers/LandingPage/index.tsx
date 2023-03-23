21e04b772716c01a566672b483a427af6e99f93f
Tenba <tenba@shinka.network>
2023-03-23 13:44:19 +0700

    fix: proper background ratio

--- containers/LandingPage/index.tsx
+++ containers/LandingPage/index.tsx
@@ -1,3 +1,4 @@
+import WithAspectRatio from "../../components/WithAspectRatio";
 import FooterPanel from "../PageHome/containers/FooterPanel";
 
 import KoloursTypo from "./components/KoloursTypo";
@@ -8,6 +9,7 @@ import { useLandingPageColor } from "./hooks/useLandingPageColor";
 import background from "./image/background.png";
 import styles from "./index.module.scss";
 
+import ImageView from "@/modules/teiki-components/components/ImageView";
 import TeikiHead from "@/modules/teiki-components/components/TeikiHead";
 import Divider from "@/modules/teiki-ui/components/Divider";
 
@@ -19,14 +21,20 @@ export default function LandingPage() {
       <div className={styles.container}>
         <RemindBar />
         <div className={styles.main}>
-          <div
-            className={styles.backgroundContainer}
-            style={{ backgroundImage: `url(${background.src}` }}
-          >
-            <div className={styles.backgroundContent}>
-              <LogoKreateWhite />
-              <Divider.Vertical color="white-10" />
-              <KoloursTypo />
+          <div className={styles.backgroundContainer}>
+            <WithAspectRatio aspectRatio={8 / 5}>
+              <ImageView
+                src={background.src}
+                style={{ width: "100%", height: "100%" }}
+                crop={{ x: 0, y: 0, w: 1, h: 1 }}
+              />
+            </WithAspectRatio>
+            <div className={styles.backgroundContentContainer}>
+              <div className={styles.backgroundContent}>
+                <LogoKreateWhite />
+                <Divider.Vertical color="white-10" />
+                <KoloursTypo />
+              </div>
             </div>
           </div>
           <div className={styles.mainContent}>
