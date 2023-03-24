0bc3cc1dc500be0de9252ebade2ff97e28a73c40
Tenba <tenba@shinka.network>
2023-03-24 17:37:01 +0700

    fix: implement AssetView ZeroFractionDigits

--- modules/teiki-ui/components/AssetViewer/index.tsx
+++ modules/teiki-ui/components/AssetViewer/index.tsx
@@ -1,5 +1,6 @@
 import { Ada$Compact } from "./components/Ada$Compact";
 import { Ada$Standard } from "./components/Ada$Standard";
+import { Ada$ZeroFractionDigits } from "./components/Ada$ZeroFractionDigits";
 import { Teiki$Standard } from "./components/Teiki$Standard";
 import { Usd$FromAda } from "./components/Usd$FromAda";
 import { Usd$FromTeiki } from "./components/Usd$FromTeiki";
@@ -8,6 +9,7 @@ const AssetViewer = {
   Ada: {
     Standard: Ada$Standard,
     Compact: Ada$Compact,
+    ZeroFractionDigits: Ada$ZeroFractionDigits,
   },
   Teiki: {
     Standard: Teiki$Standard,
