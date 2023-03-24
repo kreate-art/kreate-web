12aec78f3498b8d0a03e15f43a5f41b56f512648
Umiuma <umiuma@shinka.network>
2023-03-24 12:05:45 +0700

    feat: improve NftCardGrid

---
+++ containers/PageKolours/containers/NftCardGrid/icons/IconSort.tsx
@@ -0,0 +1,40 @@
+export default function IconSort() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M9.16797 13.3335L6.66797 15.8335L4.16797 13.3335"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.832 6.6665L13.332 4.1665L15.832 6.6665"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.332 15.8332L13.332 4.1665"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.66797 4.1665V15.8332"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
