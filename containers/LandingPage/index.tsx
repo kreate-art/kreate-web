b51f68a06bc5a05ceee9d99655990f51d6fec633
Tenba <tenba@shinka.network>
2023-03-28 16:20:13 +0700

    fix: remove redundant class

--- containers/LandingPage/index.tsx
+++ containers/LandingPage/index.tsx
@@ -7,7 +7,6 @@ import IconViewGallery from "./components/IconViewGallery";
 import Guideline from "./containers/Guideline";
 import KreataverseStory from "./containers/KreataverseStory";
 import NavBar from "./containers/NavBar";
-import LogoKreateWhite from "./containers/NavBar/components/LogoKreateWhite";
 import NftCollection from "./containers/NftCollection";
 import IconSwatches from "./containers/NftCollection/components/IconSwatches";
 import RewardSection from "./containers/RewardsSection";
@@ -18,7 +17,6 @@ import styles from "./index.module.scss";
 
 import { HOST } from "@/modules/env/client";
 import TeikiHead from "@/modules/teiki-components/components/TeikiHead";
-import Divider from "@/modules/teiki-ui/components/Divider";
 import Typography from "@/modules/teiki-ui/components/Typography";
 
 export default function LandingPage() {
