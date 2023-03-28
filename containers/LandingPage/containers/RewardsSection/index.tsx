5e22847366b333f61afd1650bca012bcbb0963aa
Tenba <tenba@shinka.network>
2023-03-28 22:19:43 +0700

    feat: refine reward section

--- containers/LandingPage/containers/RewardsSection/index.tsx
+++ containers/LandingPage/containers/RewardsSection/index.tsx
@@ -2,12 +2,15 @@ import cx from "classnames";
 import Image from "next/image";
 import Link from "next/link";
 
+import IconBulletPoint from "./components/IconBulletPoint";
 import humanFigure1 from "./images/human-figure-1.png";
 import humanFigure2 from "./images/human-figure-2.png";
 import kreationNft from "./images/kreation-nft.png";
 import styles from "./index.module.scss";
 
-import ImageView from "@/modules/teiki-components/components/ImageView";
+import Carousel from "@/modules/teiki-components/components/Carousel";
+import Divider from "@/modules/teiki-ui/components/Divider";
+import Flex from "@/modules/teiki-ui/components/Flex";
 import Typography from "@/modules/teiki-ui/components/Typography";
 
 type Props = {
@@ -29,159 +32,224 @@ export default function RewardSection({ className, style }: Props) {
     "#D6AEBC",
   ];
   return (
-    <div id="rewards" className={className} style={style}>
+    <div className={className} style={style} id="reward">
       <div className={styles.rewardsTitle}>Your Rewards</div>
-      <div className={styles.gridContainer}>
-        <div className={styles.upperGrid}>
-          <div className={styles.grid}>
+      <Carousel gap="medium" maxItemWidth={1328} style={{ marginTop: "32px" }}>
+        <div className={styles.itemCarouselContainer}>
+          <div className={styles.upperItem}>
             <Typography.Div
-              color="none"
-              size="heading2"
-              style={{ color: "#EC5929" }}
+              color="white"
               content="Kolour NFT"
+              size="heading2"
             />
-            <div className={styles.kolourList}>
-              {kolours.map((kolour) => (
-                <div
-                  key={kolour}
-                  className={styles.kolour}
-                  style={{ backgroundColor: kolour }}
+            <Flex.Row flexWrap="wrap" paddingTop="32px">
+              <Flex.Col
+                flex="1 1 0"
+                padding="0 32px 32px 0px"
+                gap="24px"
+                minWidth="500px"
+              >
+                <div className={styles.kolourList}>
+                  {kolours.map((kolour) => (
+                    <div
+                      key={kolour}
+                      className={styles.kolour}
+                      style={{ backgroundColor: kolour }}
+                    />
+                  ))}
+                </div>
+                <Image
+                  className={styles.figure}
+                  src={humanFigure1}
+                  alt="human-figure-1"
                 />
-              ))}
-            </div>
-            <div className={styles.infoCardContainer}>
-              <div className={cx(styles.infoCard, styles.gray)}>
+              </Flex.Col>
+              <Divider.Vertical color="white-10" />
+              <Flex.Col
+                flex="1 1 0"
+                padding="0px 0px 32px 32px"
+                minWidth="500px"
+              >
+                <div className={cx(styles.subTitle, styles.white)}>
+                  <IconBulletPoint />
+                  <span>Kolour the Kreataverse</span>
+                </div>
                 <Typography.Div
-                  size="heading3"
-                  color="white"
-                  content="Kolour the Kreataverse"
-                />
-                <div className={styles.infoBody}>
+                  color="white50"
+                  size="body"
+                  style={{ marginTop: "16px" }}
+                >
                   Only minted kolours can be used for Kreator studio, merch and
                   new NFT collections.
-                </div>
-              </div>
-              <div className={cx(styles.infoCard, styles.green)}>
-                <Typography.Div
-                  size="heading3"
-                  color="white"
-                  content="Royalty"
+                </Typography.Div>
+                <Divider.Horizontal
+                  className={styles.divider}
+                  color="white-10"
                 />
-                <div className={styles.infoBody}>
+                <div className={cx(styles.subTitle, styles.orange)}>
+                  <IconBulletPoint />
+                  <span>Royalty</span>
+                </div>
+                <Typography.Div
+                  color="white50"
+                  size="body"
+                  style={{ marginTop: "16px" }}
+                >
                   Get a % minting fee from future NFTs that use your kolours.
-                </div>
-              </div>
-              <div className={cx(styles.infoCard, styles.gray)}>
-                <Typography.Div
-                  size="heading3"
-                  color="white"
-                  content="Early Kreator NFT"
+                </Typography.Div>
+                <Divider.Horizontal
+                  className={styles.divider}
+                  color="white-10"
                 />
-                <div className={styles.infoBody}>
+                <div className={cx(styles.subTitle, styles.white)}>
+                  <IconBulletPoint />
+                  <span>Early Kreator NFT</span>
+                </div>
+                <Typography.Div
+                  color="white50"
+                  size="body"
+                  style={{ marginTop: "16px" }}
+                >
                   Kolour NFT holders are eligible for early-minting a Kreator
                   NFT.
-                </div>
-                <div>
+                </Typography.Div>
+              </Flex.Col>
+            </Flex.Row>
+          </div>
+          <Divider.Horizontal color="white-10" />
+          <Flex.Col padding="32px 80px">
+            <Typography.Div color="white50">
+              <Typography.Span content="Kolour and Genesis Kreation NFT holders will " />
+              <Typography.Span color="white" content="get a $KREATE airdrop" />
+              <Typography.Span content=" proportionally to their holding value." />
+            </Typography.Div>
+            <Typography.Div color="white50">
+              <Typography.Span content="$KREATE is the utility token of the " />
+              <Link href="/">
+                <span className={styles.link}>Kreate platform</span>
+              </Link>
+              <Typography.Span content=" and is highly beneficial to Kreators and Members." />
+            </Typography.Div>
+            <Typography.Div color="white50">
+              <Typography.Span content="Follow us on " />
+              <Link href="https://twitter.com/KreatePlatform">
+                <span className={styles.link}>Twitter</span>
+              </Link>
+              <Typography.Span content=" and " />
+              <Link href="https://discord.gg/kreate">
+                <span className={styles.link}>Discord</span>
+              </Link>
+              <Typography.Span content=" for the $KREATE campaign in April 2023." />
+            </Typography.Div>
+          </Flex.Col>
+        </div>
+        <div className={styles.itemCarouselContainer}>
+          <div className={styles.upperItem}>
+            <Typography.Div
+              color="white"
+              content="Genesis Kreation NFT"
+              size="heading2"
+            />
+            <Flex.Row flexWrap="wrap" paddingTop="32px">
+              <Flex.Col
+                flex="1 1 0"
+                padding="0 32px 32px 0px"
+                gap="24px"
+                minWidth="500px"
+              >
+                <div className={styles.kreationNftContainer}>
                   <Image
-                    className={styles.figure}
-                    src={humanFigure1}
-                    alt="human-figure-1"
+                    src={kreationNft}
+                    alt="kreation-nft"
+                    className={styles.kreationNft}
                   />
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.grid} style={{ gridArea: "b" }}>
-            <Typography.Div
-              size="heading2"
-              color="none"
-              style={{ color: "#EC5929" }}
-              content="Genesis Kreation NFT"
-            />
-            <div className={styles.kreationNftContainer}>
-              <ImageView
-                src={kreationNft.src}
-                className={styles.imageView}
-                crop={{ x: 0, y: 0, w: 1, h: 1 }}
-              />
-            </div>
-            <div className={styles.infoCardContainer}>
-              <div className={cx(styles.infoCard, styles.gray)}>
-                <Typography.Div
-                  size="heading3"
-                  color="white"
-                  content="Kreate your Region"
+                <Image
+                  className={styles.figure}
+                  src={humanFigure2}
+                  alt="human-figure-2"
                 />
-                <div className={styles.infoBody}>
+              </Flex.Col>
+              <Divider.Vertical color="white-10" />
+              <Flex.Col
+                flex="1 1 0"
+                padding="0px 0px 32px 32px"
+                minWidth="500px"
+              >
+                <div className={cx(styles.subTitle, styles.white)}>
+                  <IconBulletPoint />
+                  <span>Kreate your Region</span>
+                </div>
+                <Typography.Div
+                  color="white50"
+                  size="body"
+                  style={{ marginTop: "16px" }}
+                >
                   Name the region you now own. You can build it in 3D once the
                   Kreataverse is live in Q4/2023.
-                </div>
-              </div>
-              <div className={cx(styles.infoCard, styles.green)}>
-                <Typography.Div
-                  size="heading3"
-                  color="white"
-                  content="Royalty"
+                </Typography.Div>
+                <Divider.Horizontal
+                  className={styles.divider}
+                  color="white-10"
                 />
-                <div className={styles.infoBody}>
+                <div className={cx(styles.subTitle, styles.orange)}>
+                  <IconBulletPoint />
+                  <span>Royalty</span>
+                </div>
+                <Typography.Div
+                  color="white50"
+                  size="body"
+                  style={{ marginTop: "16px" }}
+                >
                   Get a % revenue share from Kreator Studios, businesses, and
                   ads on your land.
-                </div>
-              </div>
-              <div className={cx(styles.infoCard, styles.gray)}>
-                <Typography.Div
-                  size="heading3"
-                  color="white"
-                  content="Free Kreator NFT"
+                </Typography.Div>
+                <Divider.Horizontal
+                  className={styles.divider}
+                  color="white-10"
                 />
-                <div className={styles.infoBody}>
+                <div className={cx(styles.subTitle, styles.white)}>
+                  <IconBulletPoint />
+                  <span>Free Kreator NFT</span>
+                </div>
+                <Typography.Div
+                  color="white50"
+                  size="body"
+                  style={{ marginTop: "16px" }}
+                >
                   Genesis Kreation NFT holders get a FREE Kreator NFT.
-                </div>
-                <div>
-                  <Image
-                    className={styles.figure}
-                    src={humanFigure2}
-                    alt="human-figure-2"
-                  />
-                </div>
-              </div>
-            </div>
+                </Typography.Div>
+              </Flex.Col>
+            </Flex.Row>
           </div>
+          <Divider.Horizontal color="white-10" />
+          <Flex.Col padding="32px 80px">
+            <Typography.Div color="white50">
+              <Typography.Span content="Kolour and Genesis Kreation NFT holders will " />
+              <Typography.Span color="white" content="get a $KREATE airdrop" />
+              <Typography.Span content=" proportionally to their holding value." />
+            </Typography.Div>
+            <Typography.Div color="white50">
+              <Typography.Span content="$KREATE is the utility token of the " />
+              <Link href="/">
+                <span className={styles.link}>Kreate platform</span>
+              </Link>
+              <Typography.Span content=" and is highly beneficial to Kreators and Members." />
+            </Typography.Div>
+            <Typography.Div color="white50">
+              <Typography.Span content="Follow us on " />
+              <Link href="https://twitter.com/KreatePlatform">
+                <span className={styles.link}>Twitter</span>
+              </Link>
+              <Typography.Span content=" and " />
+              <Link href="https://discord.gg/kreate">
+                <span className={styles.link}>Discord</span>
+              </Link>
+              <Typography.Span content=" for the $KREATE campaign in April 2023." />
+            </Typography.Div>
+          </Flex.Col>
         </div>
-        <div
-          className={cx(styles.grid, styles.bottomGrid)}
-          style={{ gridArea: "c" }}
-        >
-          <Typography.Div color="white" className={styles.infoBottomGrid}>
-            <div>
-              Kolour and Genesis Kreation NFT holders will{" "}
-              <b>get a $KREATE airdrop </b>
-              proportionally to their holding value.
-            </div>
-            <div>
-              $KREATE is the utility token of the{" "}
-              <Link className={styles.link} href="https://kreate.community/">
-                Kreate platform
-              </Link>{" "}
-              and is highly beneficial to Kreators and Fans.
-            </div>
-            <div>
-              Follow us on{" "}
-              <Link
-                className={styles.link}
-                href="https://twitter.com/KreatePlatform"
-              >
-                Twitter
-              </Link>{" "}
-              and{" "}
-              <Link className={styles.link} href="https://discord.gg/kreate">
-                Discord
-              </Link>{" "}
-              for the $KREATE campaign in April 2023.
-            </div>
-          </Typography.Div>
-        </div>
-      </div>
+      </Carousel>
     </div>
   );
 }
