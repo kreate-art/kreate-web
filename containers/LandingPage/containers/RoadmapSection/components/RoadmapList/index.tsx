360ac499de81cf88e671ea413f90dc8d4986f24e
Tenba <tenba@shinka.network>
2023-03-28 20:13:40 +0700

    fix: refine roadmap

--- containers/LandingPage/containers/RoadmapSection/components/RoadmapList/index.tsx
+++ containers/LandingPage/containers/RoadmapSection/components/RoadmapList/index.tsx
@@ -2,7 +2,7 @@ import cx from "classnames";
 import * as React from "react";
 
 import IconCompleted from "../../icons/IconCompleted";
-import IconUncompleted from "../../icons/IconUncompleted";
+import IconRunning from "../../icons/IconRunning";
 
 import styles from "./index.module.scss";
 
@@ -37,11 +37,38 @@ function Uncompleted({
 }) {
   return (
     <li className={cx(styles.uncompleted, className)} style={style}>
+      <span>{children}</span>
+    </li>
+  );
+}
+
+function Running({
+  className,
+  style,
+  content,
+  children = content,
+}: {
+  className?: string;
+  style?: React.CSSProperties;
+  content?: React.ReactNode;
+  children?: React.ReactNode;
+}) {
+  return (
+    <li className={cx(styles.running, className)} style={style}>
       <Flex.Row gap="10px">
-        <Flex.Row flex="0 0 auto">
-          <IconUncompleted />
+        <Flex.Row
+          flex="0 0 auto"
+          padding="4px 8px"
+          justifyContent="center"
+          gap="8px"
+          className={cx(styles.statusCard, styles.running)}
+        >
+          <IconRunning />
+          <span>Running</span>
         </Flex.Row>
-        <Flex.Cell flex="1 1 0">{children}</Flex.Cell>
+        <Flex.Row flex="1 1 0" className={styles.children}>
+          {children}
+        </Flex.Row>
       </Flex.Row>
     </li>
   );
@@ -61,15 +88,24 @@ function Completed({
   return (
     <li className={cx(styles.completed, className)} style={style}>
       <Flex.Row gap="10px">
-        <Flex.Row flex="0 0 auto">
+        <Flex.Row
+          flex="0 0 auto"
+          padding="4px 8px"
+          justifyContent="center"
+          gap="8px"
+          className={cx(styles.statusCard, styles.completed)}
+        >
           <IconCompleted />
+          <span>Completed</span>
         </Flex.Row>
-        <Flex.Cell flex="1 1 0">{children}</Flex.Cell>
+        <Flex.Row flex="1 1 0" className={styles.children}>
+          {children}
+        </Flex.Row>
       </Flex.Row>
     </li>
   );
 }
 
-const RoadmapList = Object.assign(Root, { Uncompleted, Completed });
+const RoadmapList = Object.assign(Root, { Uncompleted, Completed, Running });
 
 export default RoadmapList;
--- containers/LandingPage/containers/RoadmapSection/icons/IconCompleted.tsx
+++ containers/LandingPage/containers/RoadmapSection/icons/IconCompleted.tsx
@@ -3,16 +3,16 @@ import { SVGProps } from "react";
 export default function IconCompleted(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect width="32" height="32" rx="16" fill="#00A167" />
+      <rect y="0.5" width="24" height="24" rx="12" fill="#00A167" />
       <path
-        d="M10 16.0005L14.2426 20.2431L22.7279 11.7578"
+        d="M8 12.5003L10.8284 15.3287L16.4853 9.67188"
         stroke="white"
         strokeWidth="2"
         strokeLinecap="round"
