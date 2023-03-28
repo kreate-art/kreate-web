6997a0f71cd05ec3c43acab466dc215555c4f71b
sk-yagi <yagi@shinka.network>
2023-03-28 20:38:19 +0700

    feat: add indicator for kreataverse story

--- containers/LandingPage/containers/KreataverseStory/index.tsx
+++ containers/LandingPage/containers/KreataverseStory/index.tsx
@@ -1,8 +1,10 @@
 import cx from "classnames";
 
-import LeftStory from "./components/LeftStory";
+import { LeftStory$1, LeftStory$2, LeftStory$3 } from "./components/LeftStory";
 import RightStory from "./components/RightStory";
 import styles from "./index.module.scss";
+
+import Carousel from "@/modules/teiki-components/components/Carousel";
 
 type Props = {
   className?: string;
@@ -14,8 +16,23 @@ export default function KreataverseStory({ className, style }: Props) {
     <div id="kreataverse_story">
       <div className={styles.story}>Kreataverse Story</div>
       <div className={cx(styles.main, className)} style={style}>
-        <LeftStory />
-        <RightStory />
+        {/* TODO: Refine me!! */}
+        <Carousel className={styles.carousel} indicatorPosition="bottom-left">
+          <>
+            <LeftStory$1 />
+            <RightStory />
+          </>
+          <>
+            <LeftStory$2 />
+            {/* As discussed with our designer, these banners should be unique */}
+            <RightStory />
+          </>
+          <>
+            <LeftStory$3 />
+            {/* As discussed with our designer, these banners should be unique */}
+            <RightStory />
+          </>
+        </Carousel>
       </div>
     </div>
   );
