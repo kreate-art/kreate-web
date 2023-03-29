161b4c4b75f09eb236e89a9f37218138f6c9ebac
Shishi | Kreate <shishi@shinka.network>
2023-03-29 15:08:03 +0700

    feat: normalize genesis kreation palette

--- modules/kolours/genesis-kreation.ts
+++ modules/kolours/genesis-kreation.ts
@@ -3,11 +3,9 @@ import {
   GenesisKreationEntry,
   GenesisKreationId,
   GenesisKreationStatus,
-  Kolour,
   Layer,
 } from "./types/Kolours";
 
-import { assert } from "@/modules/common-utils";
 import { postgres, Sql } from "@/modules/next-backend/db";
 import { Lovelace } from "@/modules/next-backend/types";
 import { getIpfsUrl } from "@/modules/urls";
@@ -18,13 +16,12 @@ type GenesisKreationDbRow = {
   finalImageCid: string;
   baseFee: bigint;
   createdAt: Date;
-  palette: { k: Kolour; l: string }[];
-  bookStatus: "booked" | "minted" | null;
   userAddress: string | null;
   fee: bigint | null;
   name: string | null;
   description: string | null;
-  koloursStatus: ("booked" | "minted" | "NULL" | null)[]; // SQL is dumb
+  status: GenesisKreationStatus;
+  palette: string; // kolour|cid|status,...
 };
 
 export async function getAllGenesisKreations(
@@ -39,23 +36,21 @@ export async function getAllGenesisKreations(
       gl.final_image_cid,
       gl.listed_fee AS base_fee,
       gl.created_at,
-      gl.palette,
-      gb.status AS book_status,
       gb.user_address,
       gb.fee,
       gb.name,
       gb.description,
-      array_agg(kb.status ORDER BY p.i) kolours_status
+      coalesce(gb.status::text, CASE WHEN bool_and(kb.status IS NOT DISTINCT FROM 'minted') THEN 'ready' ELSE 'unready' END) AS status,
+      string_agg(concat(gp.kolour, '|', gp.layer_image_cid, '|', kb.status), ',' ORDER BY gp.id) AS palette
     FROM
       kolours.genesis_kreation_list gl
       LEFT JOIN kolours.genesis_kreation_book gb
         ON gl.kreation = gb.kreation
           AND gb.status <> 'expired'
-      CROSS JOIN LATERAL
-        ROWS FROM (jsonb_to_recordset(gl.palette) AS (k varchar(6)))
-        WITH ORDINALITY p(k, i)
+      INNER JOIN kolours.genesis_kreation_palette gp
+        ON gp.kreation_id = gl.id
       LEFT JOIN kolours.kolour_book kb
-        ON kb.kolour = p.k
+        ON kb.kolour = gp.kolour
           AND kb.status <> 'expired'
     GROUP BY
       gl.id,
@@ -64,38 +59,27 @@ export async function getAllGenesisKreations(
       gl.final_image_cid,
       gl.listed_fee,
       gl.created_at,
-      gl.palette,
-      gb.status,
       gb.user_address,
       gb.fee,
       gb.name,
-      gb.description
+      gb.description,
+      gb.status
     ORDER BY
       gl.id ASC
   `;
   return rows.map((row) => {
-    const r_palette = row.palette;
-    const r_koStatus = row.koloursStatus;
-    assert(r_palette.length === r_koStatus.length, "palette length not match");
-    const r_status = row.bookStatus;
-
-    const koStatus = r_koStatus.map((s) => (!s || s === "NULL" ? "free" : s));
-    const status = r_status
-      ? r_status
-      : koStatus.every((s) => s === "minted")
-      ? "ready"
-      : "unready";
-    const palette = r_palette.map(({ k, l }, i): Layer => {
+    const palette = row.palette.split(",").map((text): Layer => {
+      const [kolour, cid, status] = text.split("|");
       return {
-        kolour: k,
-        image: { src: getIpfsUrl(l) },
-        status: koStatus[i],
-        ...computeFees(calculateKolourFee(k), discount),
+        kolour,
+        image: { src: getIpfsUrl(cid) },
+        status: (status as "booked" | "minted" | "") || "free",
+        ...computeFees(calculateKolourFee(kolour), discount),
       };
     });
     return {
       id: row.kreation,
-      status,
+      status: row.status,
       initialImage: { src: getIpfsUrl(row.initialImageCid) },
       finalImage: { src: getIpfsUrl(row.finalImageCid) },
       ...computeFees(row.baseFee, discount),
@@ -139,20 +123,21 @@ async function queryGenesisKreation<T extends object = Record<string, never>>(
   sql: Sql,
   id: GenesisKreationId,
   extraSelect?: postgres.Fragment
-): Promise<(T & StatusOutField) | null> {
-  const [row]: [(T & StatusInFields)?] = await sql`
+): Promise<(T & { status: GenesisKreationStatus }) | null> {
+  const [row]: [(T & { status: GenesisKreationStatus })?] = await sql`
     SELECT
       ${extraSelect ?? sql``}
-      gb.status AS book_status,
-      (
+      coalesce(gb.status::text, (
         SELECT
-          bool_and(kb.id IS NOT NULL)
+          CASE WHEN bool_and(kb.id IS NOT NULL) THEN 'ready' ELSE 'unready' END
         FROM
-          jsonb_to_recordset(gl.palette) AS p(k varchar(6))
+          kolours.genesis_kreation_palette gp
           LEFT JOIN kolours.kolour_book kb
-            ON kb.kolour = p.k
+            ON kb.kolour = gp.kolour
               AND kb.status = 'minted'
-      ) AS is_ready
+        WHERE
+          gp.kreation_id = gl.id
+      ))
     FROM
       kolours.genesis_kreation_list gl
       LEFT JOIN kolours.genesis_kreation_book gb
@@ -161,21 +146,5 @@ async function queryGenesisKreation<T extends object = Record<string, never>>(
     WHERE
       gl.kreation = ${id}
   `;
-  if (row) {
-    const { bookStatus, isReady, ...rest } = row;
-    return {
-      ...rest,
-      status: bookStatus == null ? (isReady ? "ready" : "unready") : bookStatus,
-    } as T & StatusOutField;
-  } else {
-    return null;
-  }
+  return row ?? null;
 }
-
-type StatusInFields = {
-  bookStatus: "booked" | "minted" | null;
-  isReady: boolean;
-};
-type StatusOutField = {
-  status: GenesisKreationStatus;
-};
