9a7eceb69b4538c100d23eaec91e3c26b5a81414
Umiuma <umiuma@shinka.network>
2023-03-31 14:44:19 +0700

    feat: style remaning quota label

---
+++ containers/PageMintByColorPicker/icons/IconGift.tsx
@@ -0,0 +1,54 @@
+export default function IconGift() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M19 12H13C12.4477 12 12 12.4477 12 13V20H19C19.5523 20 20 19.5523 20 19V13C20 12.4477 19.5523 12 19 12Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11 12H5C4.44772 12 4 12.4477 4 13V19C4 19.5523 4.44772 20 5 20H12V13C12 12.4477 11.5523 12 11 12Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20 8H13C12.4477 8 12 8.44772 12 9V11C12 11.5523 12.4477 12 13 12H20C20.5523 12 21 11.5523 21 11V9C21 8.44772 20.5523 8 20 8Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11 8H4C3.44772 8 3 8.44772 3 9V11C3 11.5523 3.44772 12 4 12H11C11.5523 12 12 11.5523 12 11V9C12 8.44772 11.5523 8 11 8Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 8V5.5C12 4.11929 13.1193 3 14.5 3C15.8807 3 17 4.11929 17 5.5C17 6.88071 15.8807 8 14.5 8H12Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 8V5.5C12 4.11929 10.8807 3 9.5 3C8.11929 3 7 4.11929 7 5.5C7 6.88071 8.11929 8 9.5 8H12Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
