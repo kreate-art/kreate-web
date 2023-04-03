6cd1dbb876ea63b69363068844a101cc4d0be529
Shishi | Kreate <shishi@shinka.network>
2023-04-03 18:47:37 +0700

    refactor: low hanging fruit

--- pages/api/v1/ada-price.ts
+++ pages/api/v1/ada-price.ts
@@ -3,14 +3,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { apiCatch } from "@/modules/next-backend/api/errors";
 import { sendJson } from "@/modules/next-backend/api/helpers";
 import { redis } from "@/modules/next-backend/connections";
-import { getAdaPriceInfo } from "@/modules/next-backend/logic/getAdaPriceInfo";
+import { lookupAdaPriceInfo } from "@/modules/next-backend/logic/getAdaPriceInfo";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const response = await getAdaPriceInfo(redis);
+    const { status, response } = await lookupAdaPriceInfo(redis);
+    res.setHeader("X-Kreate-Cache", status);
     sendJson(res.status(200), response);
   } catch (error) {
     apiCatch(req, res, error);
